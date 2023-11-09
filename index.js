14d65dcdbb8a5f4b2ad9e914bb351fed8328fd5e
ShatteredDisk <73539907+ShatteredDisk@users.noreply.github.com>
2023-11-09 15:22:20 -0600

    Update index.js

--- index.js
+++ index.js
@@ -35,11 +35,11 @@ function submit() {
     })
         .then((response) => response.json())
         .then((result) => {
-            navigator.clipboard.writeText(result);
+            navigator.clipboard.writeText(JSON.stringify(result));
             console.log('Result: ' + result);
         })
         .catch((error) => {
-            navigator.clipboard.writeText(error);
+            navigator.clipboard.writeText(JSON.stringify(error));
             console.error('Error: ' + error);
         });
 }
